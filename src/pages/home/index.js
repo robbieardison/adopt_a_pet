67e756d23edce2c058eb076f80907ee342b6d183
roger cassano <robbieardison@Robbies-MacBook-Pro.local>
2022-09-11 17:20:01 +0700

    changed Link

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { getPets } from '../../api/petfinder';
 import Hero from '../../components/hero';
 import Pet from '../../components/pet';
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const HomePage = () => {
   const [data, setData] = useState(null);
@@ -32,11 +32,10 @@ const HomePage = () => {
       {data.length ? (
         <div className="grid">
           {data.map((animal) => (
-            <a // Change me to a Link!
+            <Link // Change me to a Link!
               key={animal.id}
-              href={`/${animal.type.toLowerCase()}/${animal.id}`}
-              className="pet"
-            >
+              to={`/${animal.type.toLowerCase()}/${animal.id}`}
+              className="pet">
               <article>
                 <div className="pet-image-container">
                   {
@@ -55,7 +54,7 @@ const HomePage = () => {
                 <p>Color: {animal.colors.primary}</p>
                 <p>Gender: {animal.gender}</p>
               </article>
-            </a> // Don't forget to change me!
+            </Link> // Don't forget to change me!
           ))}
         </div>
       ) : (
